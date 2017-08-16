34aae9e0befb98e4910a7ce4300626fc4bc8e5f9
Jeff S <jdstein1@gmail.com>
2017-08-16 17:12:54 -0500

    flesh out page structure...
    add components & styling

---
+++ server.js
@@ -0,0 +1,15 @@
+/* server.js */
+
+const express = require('express')
+const path = require('path')
+const app = express()
+
+console.log('__dirname: ', __dirname)
+
+app.use(express.static('src'))
+// app.use('/public', express.static(__dirname + 'src'))
+// app.use('/', express.static(path.join(__dirname, 'src')))
+
+app.listen(1138, function () {
+  console.log('ready port 1138!')
+})
